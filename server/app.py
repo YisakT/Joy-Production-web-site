64c559fc12e6aa571fb9b3d4d04a2c0695db0ad2
Yisak <yisakbruck23@gmail.com>
2023-10-02 15:12:34 -0400

    models starter code

---
+++ server/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import os
+from dotenv import load_dotenv
+from models import db, Customer, Project, Invoice, Feedback, Portfolio, Booking, License, Contract, Equipment, Employee
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Create a Flask app
+app = Flask(__name__)
+
+# Configure the Flask app with your database URL from .env
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+# Initialize the SQLAlchemy database
+db.init_app(app)
+
+# Define your Flask routes here
+
+# Define a simple route for testing
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
